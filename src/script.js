4fc44add71c48f8c0f2b72b3a3623a161db8bfb7
paucud <102471797+paucud@users.noreply.github.com>
2022-07-23 21:50:38 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+// funkcja wyświetlająca imię i nazwisko
+const showName = () => {
+    const user = document.getElementById("userName");
+    user.textContent = "Paulina Cudak"; // wyświetlane imię i nazwisko
+};
+showName();
